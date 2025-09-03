8423d11905ae28c1ef3d260fd9c5fdc193f419f9
Tuong1994 <nhambontuong68@gmail.com>
2025-09-03 17:02:23 +0700

    update Header Search init

--- src/components/Page/Header/Bottom/HeaderSearch.tsx
+++ src/components/Page/Header/Bottom/HeaderSearch.tsx
@@ -1,11 +1,14 @@
-import { FC, KeyboardEvent, useState } from "react";
+import { FC, KeyboardEvent, useRef, useState } from "react";
+import { Image, Typography } from "@/components/UI";
 import { Input } from "@/components/Control";
 import { FaSearch } from "react-icons/fa";
 import { useRouter } from "next/router";
-import { useLang, useViewpoint } from "@/hooks";
+import { useClickOutside, useLang, useRender, useViewpoint } from "@/hooks";
 import { LIST_LIMIT_ITEMS } from "@/services/helper";
 import url from "@/common/constant/url";
 import utils from "@/utils";
+
+const { Paragraph } = Typography;
 
 const { SEARCH } = url;
 
@@ -18,16 +21,31 @@ const HeaderSearch: FC<HeaderSearchProps> = () => {
 
   const [keywords, setKeywords] = useState<string>("");
 
+  const [dropdown, setDropdown] = useState<boolean>(false);
+
+  const searchRef = useRef<HTMLDivElement | null>(null)
+
   const router = useRouter();
 
-  const responsive = isPhone || isSmTablet
+  const render = useRender(dropdown)
 
-  const inputSize = responsive ? 'md' : 'lg'
+  useClickOutside(searchRef, setDropdown)
+
+  const responsive = isPhone || isSmTablet;
+
+  const inputSize = responsive ? "md" : "lg";
 
   const disabledClassName = !keywords ? "search-icon-disabled" : "";
 
+  const activeClassName = dropdown ? "search-dropdown-active" : "";
+
+  const dropdownClassName = utils.formatClassName("search-dropdown", activeClassName);
+
   const handleNavigate = () => {
-    router.push({ pathname: SEARCH, query: { page: 1, limit: LIST_LIMIT_ITEMS, keywords, langCode: locale } });
+    router.push({
+      pathname: SEARCH,
+      query: { page: 1, limit: LIST_LIMIT_ITEMS, keywords, langCode: locale },
+    });
     setKeywords("");
   };
 
@@ -35,6 +53,17 @@ const HeaderSearch: FC<HeaderSearchProps> = () => {
 
   const handleKeyPress = (e: KeyboardEvent) => {
     if (e.key === "Enter") handleNavigate();
+  };
+
+  const handleOpenDropdown = () => setDropdown(true);
+
+  const renderProductItems = () => {
+    return [...Array(10)].map((_, idx) => (
+      <div key={idx} className="dropdown-item">
+        <Image imgWidth={60} imgHeight={60} />
+        <Paragraph rootClassName="item-name">Product name</Paragraph>
+      </div>
+    ));
   };
 
   const searchIcon = (
@@ -49,16 +78,18 @@ const HeaderSearch: FC<HeaderSearchProps> = () => {
   );
 
   return (
-    <Input
-      color="green"
-      rootClassName="bottom-search"
-      sizes={inputSize}
-      value={keywords}
-      addonAfter={searchIcon}
-      placeholder={lang.common.form.placeholder.search}
-      onKeyDown={handleKeyPress}
-      onChangeInput={handleChangeInput}
-    />
+    <div ref={searchRef} className="bottom-search" onFocus={handleOpenDropdown}>
+      <Input
+        color="green"
+        sizes={inputSize}
+        value={keywords}
+        addonAfter={searchIcon}
+        placeholder={lang.common.form.placeholder.search}
+        onKeyDown={handleKeyPress}
+        onChangeInput={handleChangeInput}
+      />
+      {render && <div className={dropdownClassName}>{renderProductItems()}</div>}
+    </div>
   );
 };
 
